3e30011c910f9519350b8542775c63a925d34bae
zhangcheng <201250187@smail.nju.edu.cn>
2024-03-29 15:28:27 +0800

    666

---
+++ src/test/java/com/nju/edu/gtms/GtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.edu.gtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
